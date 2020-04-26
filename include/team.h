23db013946c1a45814acb96ce14adaca1ca9960f
tomasanchez <tosanchez@est.frba.utn.edu.ar>
2020-04-26 20:09:02 -0300

    Updated

--- include/team.h
+++ include/team.h
@@ -1,6 +1,56 @@
 #pragma once
 
-#include <trainer.h>
+#include <connTeam.h>
+
+/*  Imported from 5 - status of process diagram  */
+typedef enum{
+    NEW,
+    READY,
+    BLOCKED,
+    EXECUTING,
+    EXIT
+} Status;
+
+/* Error list for debugging */
+typedef enum{
+    TRAINER_CREATED,
+    TRAINER_CREATION_FAILED,
+    TRAINER_DELETED,
+    TRAINER_DELETION_FAILED,
+    TRAINER_OBJECTIVES,
+    TRAINER_NO_OBJECTIVES
+} trainer_error;
+
+
+/* Objectives come as:
+ *
+ * [Pikachu|Pikachu|Squirtle|Pidgey,Pikachu|Charmander|Charmander,Squirtle|Bulbasaur]
+ * 
+ * Where between token '|' there are pokemons and ',' trainers
+ * 
+ */
+typedef struct{
+
+    char* objective;
+    u_int32_t total_objectives;
+
+} Objective;
+
+/* Trainers */
+typedef struct
+{
+    /*  About objective */
+    Objective personal;
+    int pokemons_in_hand;
+    
+    /* Process Related */
+    Status actual_status;
+
+    /*  Position    */
+    u_int32_t x_position;
+    u_int32_t y_position;
+
+} Trainer;
 
 /* TEAM */
 typedef struct 
@@ -12,3 +62,5 @@ typedef struct
     Trainer* team[];
 
 } Team;
+
+trainer_error Trainer_Create(Team* trainer);
