9da1e921e7a90ff88af13400cf971a5f6f42f88d
tomasanchez <tosanchez@est.frba.utn.edu.ar>
2020-06-06 23:56:31 -0300

    Moved files, and updated process  Team

--- Proceso Team/include/team.h
+++ Proceso Team/include/team.h
@@ -1,17 +1,24 @@
 #pragma once
 
 #include <stdlib.h>
-#include <connTeam.h>
 #include <assert.h>
 #include <inttypes.h>
 #include <commons/collections/list.h>
 #include <commons/string.h>
+#include <commons/process.h>
 #include "teamconfig.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
 
 // Just for aesthetics
 #define WORLD_POSITION world_pos
 #define PRINT_TEST 0
 #define LIBERAR 1
+
+#ifndef VERBOSE
+#define VERBOSE 1
+#endif
 
 /*  Position Vector of 32 bit    */
 typedef struct vector2D_32b
@@ -20,6 +27,10 @@ typedef struct vector2D_32b
 	uint32_t y_;
 
 } world_pos;
+
+// ============================================================================================================
+//                                            ***** TRAINERS *****
+// ============================================================================================================
 
 /*  Imported from 5 - status of process diagram  */
 typedef enum thread_status{
@@ -50,12 +61,20 @@ typedef struct trainer_list
     
     /*  Process Related  */
     Status actual_status;
-    uint32_t thread_id;
+    unsigned int thread_id;
+    uint32_t trainer_id;
 
     /*  Position    */
     WORLD_POSITION actual_position;
 } Trainer;
 
+
+/* Asigna Thread Id al Trainer*/
+void Trainer_dar_id(void* elemento);
+
+// ============================================================================================================
+//                                                ***** TEAM *****
+// ============================================================================================================
 
 /*  Team
  * =================
@@ -73,36 +92,41 @@ typedef struct process_team
         uint32_t cpu_cycle;
         uint32_t quantum;
         uint32_t initial_estimation;
+        double alpha;
+        char* planning_algorithm;
         /*  Connection related  */
         uint32_t reconnection_time;
         char* broker_IP;
         char* broker_port;
-        char* planning_algorithm;
     /* About Team */
-    uint32_t team_size;
+    int team_size;
     t_list* global_objective;
     t_list* trainers;
 } Team;
 
-/* Creates a team */
-Team* Team_create(void);
-
 /*  Initializes the TEAM PROCESS    */
 Team* Team_Init(void);
+
+/* Creates a team */
+Team* Team_create(void);
 
 /* Loads the trainers information from File Config */
 void Team_load_trainers_config( Team* team);
 
-/* Spawns Trainers */
-void Team_create_trainers(Team* team);
-
-/* Loads global objectives */
+/* Loads global configuration (Connection and planification data) */
 void Team_load_global_config(Team *this_team);
 
+/* Admite los procesos, que pasan a la cola de Ready*/
+int Team_admitir_trainers (Team* team);
+
+/* Creates Trainers threads */
+int Team_create_trainers(Team* team);
+
 /* Frees memory */
-void Team_Destroy(Team* team);
+int Team_Destroy(Team* team);
+
+/* Frees Trianers' memory and trheads (Por ahora tambien hace el join)*/
+int Team_destroy_trainers (Team* team);
 
 /* Frees Team's list trainers and list global objective*/
-void Team_destroy_lists(Team* team);
-
-char* str_objective(Team* team);
+void Team_destroy_trainers_and_objective(Team* team);
