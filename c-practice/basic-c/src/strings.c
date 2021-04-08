d05c663696c2aae927a8210132f5c575c9040f9f
Tomas <tosanchez@frba.utn.edu.ar>
2021-04-07 23:49:08 -0300

    fix: strcat saving an addition '\0'

--- c-practice/basic-c/src/strings.c
+++ c-practice/basic-c/src/strings.c
@@ -17,7 +17,7 @@
 
 size_t str_len(const char *iv_str)
 {
-    return *iv_str == '\0' ? 0lu : str_len(iv_str + 1) + 1lu;
+    return iv_str ? *iv_str == '\0' ? 0lu : str_len(iv_str + 1) + 1lu : 0lu;
 }
 
 char *str_concat(const char *iv_start, const char *iv_end)
@@ -41,8 +41,10 @@ char *str_concat(const char *iv_start, const char *iv_end)
         ev_concat[i] = iv_start[i];
 
     // Copying end
-    for (size_t i = lv_start_size + 1lu; i < lv_concat_size; i++)
-        ev_concat[i] = iv_end[i];
+    for (size_t i = lv_start_size, j = 0; i < lv_concat_size; i++, j++)
+        ev_concat[i] = iv_end[j];
+
+    puts(ev_concat);
 
     return ev_concat;
 }
